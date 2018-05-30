49a688974b05b3bd0eb472510ffc832dcee88d4d
Rafael Honorio <rafael.contatotrab@gmail.com>
2018-05-30 00:50:37 -0300

    finalizando a parte de login

--- classes/Login.class.php
+++ classes/Login.class.php
@@ -3,7 +3,25 @@
     {
         public function logar($email,$senha)
         {
-            
-        }
+            $buscar=mysql_query("SELECT * FROM usuarios WHERE email='$email' AND senha='$senha' AND status='1' LIMIT 1");
+            if(mysql_num_rows($buscar)==1)
+            {
+                $dados=mysql_fetch_array($buscar);
+                $_SESSION["email"]=$dados["email"];
+                $_SESSION['senha']=$dados['senha'];
+                $_SESSION['nivel']=$dados['nivel'];
+                setcookie("logado",1);
+                $log=1;
+            }
+            if(isset($log))
+            {
+                $flash="Você foi logado com sucesso!";
+            }
+            else
+            {
+                $flash="Ops! Digite seu e-mail e sua senha corretamente";
+            }
+            echo $flash;
+        }   
     }
 ?>
--- includes/header.php
+++ includes/header.php
@@ -67,8 +67,38 @@ if($startaction==1)
 {
    if($acao=="logar")
    {
+        //Dados
+        $email=addslashes($_POST['email']);
+        $senha=addslashes(sha1($_POST['senha']."senha"));
+
+        if(empty($email) || empty($senha))
+        {
+            $msg="Preencha todos os campos";
+        }else
+        {
+            if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+            {
+                $msg="Digite seu e-mail corretamente!";
+            }
+            else
+            {
+                //Executa a busca pelo usuário
+                $login=new Login;
+                echo "<div class=\"flash\">";
+                $login=$login->logar($email,$senha);
+                echo "</div>";
+            }
+        }
    }
 }
+
+//Metodo de checar usuário
+if(isset($_SESSION['email']) && isset($_SESSION['senha']))
+{
+    $logado=1;
+    $nivel=$_SESSION['nivel'];
+}
+
 //variaveis de estilo
 if(empty($msg)){
     $display="display:none;";
